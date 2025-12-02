4d02f590fb0b8589d770f5ffac3d89b03894fad4
Ryan Schuetzler <rschuet@gmail.com>
2025-12-01 17:36:18 -0700

    Initial commit

---
+++ lib/advent_of_code2025.ex
@@ -0,0 +1,18 @@
+defmodule AdventOfCode2025 do
+  @moduledoc """
+  Documentation for `AdventOfCode2025`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AdventOfCode2025.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
