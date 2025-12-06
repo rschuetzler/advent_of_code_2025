469d79d65694cfebefe2c4f549871874049a1fd8
Ryan Schuetzler <rschuet@gmail.com>
2025-12-06 09:04:40 -0700

    Day 5, part 1 (and starting pt 2)

--- lib/advent_of_code2025/days/day05.ex
+++ lib/advent_of_code2025/days/day05.ex
@@ -7,25 +7,60 @@ defmodule AdventOfCode2025.Days.Day05 do
 
   def solve do
     input =
-      Input.read_raw("day05-sample")
+      Input.read_raw("day05")
       |> prep_inputs()
 
-    result = part1(input)
+    result = part1(input |> make_ranges())
     IO.inspect(result, label: "Part 1")
     result2 = part2(input)
     IO.inspect(result2, label: "Part 2")
   end
 
-  def part1(inputs) do
-    inputs
+  def part1(input) do
+    input.available
+    |> Enum.filter(fn x -> is_fresh?(input, x) end)
+    |> Enum.count()
+
+    # input.available
+    # |> Enum.filter(fn x -> x in input.fresh end)
+    # |> Enum.count()
   end
 
-  def part2(inputs) do
-    inputs
+  def part2(input) do
+    input.fresh
   end
 
   def prep_inputs(input) do
-    # add_index = fn row -> Enum.with_index(row, fn element, index -> {index, element} end) end
-    input
+    fresh =
+      input
+      |> String.split("\n\n")
+      |> List.first()
+      |> String.split("\n")
+      |> Enum.map(fn x -> String.split(x, "-") |> Enum.map(fn y -> String.to_integer(y) end) end)
+
+    available =
+      input
+      |> String.split("\n\n")
+      |> List.last()
+      |> String.split("\n")
+      |> Enum.map(fn x -> String.to_integer(x) end)
+
+    %{fresh: fresh, available: available}
+  end
+
+  def make_ranges(%{fresh: fresh} = input) do
+    fresh =
+      fresh
+      |> Enum.map(fn [x, y | _] -> x..y end)
+      |> List.flatten()
+
+    %{input | fresh: fresh}
+  end
+
+  def is_fresh?(input, item) do
+    Enum.any?(input.fresh, fn x -> item in x end)
+  end
+
+  def merge_duplicates(fresh) do
   end
 end
